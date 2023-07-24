10b3e01d162f632c9d084467c395ae88b124bcaf
3chospirits <3chospirits@gmail.com>
2023-07-24 02:27:08 -0400

    add copilot completion plugins

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,4 +6,22 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({
+        suggestion = { enabled = false },
+        panel = { enabled = false },
+      })
+    end,
+  },
+  {
+    "zbirenbaum/copilot-cmp",
+    config = function ()
+      require("copilot_cmp").setup()
+    end
+  }
+  
 }
